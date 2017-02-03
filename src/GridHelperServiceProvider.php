330fb1e5779d2aedbd12d11693c977d0d2758e3e
Artur Anyszek <a.anyszek@gmail.com>
2017-02-03 15:36:45 +0100

    view added

--- src/GridHelperServiceProvider.php
+++ src/GridHelperServiceProvider.php
@@ -11,12 +11,13 @@ use Illuminate\Support\ServiceProvider;
 class GridHelperServiceProvider extends ServiceProvider {
 
     public function boot() {
+        $this->loadViewsFrom(__DIR__ . '/views', 'gridhelper');
 //        $this->publishes([
 //            __DIR__ . '/migrations' => $this->app->databasePath() . '/migrations'
 //                ], 'migrations');
-//        $this->publishes([
-//            __DIR__ . '/config' => config_path()
-//                ], 'config');
+        $this->publishes([
+            __DIR__ . '/views' => resource_path('views/vendor/gridhelper'),
+        ]);
     }
 
     public function register() {
