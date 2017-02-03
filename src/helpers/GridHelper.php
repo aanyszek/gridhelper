1f00ae41f6463f2e50fe6fd07c8d862cb69dd29a
Artur Anyszek <a.anyszek@gmail.com>
2017-02-03 15:57:50 +0100

    poprawka widoku

--- src/helpers/GridHelper.php
+++ src/helpers/GridHelper.php
@@ -2,7 +2,6 @@
 
 namespace Bunta\GridHelper\Helpers;
 
-
 use Illuminate\Support\Facades\Config;
 use Nayjest\Grids\Components\Base\RenderableRegistry;
 use Nayjest\Grids\Components\ColumnHeadersRow;
@@ -27,112 +26,105 @@ use Nayjest\Grids\FilterConfig;
 use Nayjest\Grids\Grid;
 use Nayjest\Grids\GridConfig;
 use Nayjest\Grids\SelectFilterConfig;
-
 use Nayjest\Grids\ObjectDataRow;
 
-class GridHelper
-{
-    public static function FieldConfig($name, $label)
-    {
+class GridHelper {
+
+    public static function FieldConfig($name, $label) {
         return (new FieldConfig())
-        ->setName($name)
-        ->setLabel($label)
-            ->setSortable(true);
+                        ->setName($name)
+                        ->setLabel($label)
+                        ->setSortable(true);
         // ->addFilter(
         //     (new \SelectFilterConfig)
         //         ->setOptions(['1'=>2,'3'=>'4'])
         // );
     }
 
-    public static function FieldConfigFilterLike($name, $label, $filter_field)
-    {
+    public static function FieldConfigFilterLike($name, $label, $filter_field) {
         return self::FieldConfig($name, $label)
-        ->addFilter(
-            (new FilterConfig())
-                ->setName($filter_field)
-                ->setOperator(FilterConfig::OPERATOR_LIKE)
+                        ->addFilter(
+                                (new FilterConfig())
+                                ->setName($filter_field)
+                                ->setOperator(FilterConfig::OPERATOR_LIKE)
         );
     }
 
-    public static function FieldConfigFilterOption($name, $label, $options = [])
-    {
+    public static function FieldConfigFilterOption($name, $label, $options = []) {
         return self::FieldConfig($name, $label)
-        ->addFilter(
-            (new SelectFilterConfig())
-                ->setOptions($options)
+                        ->addFilter(
+                                (new SelectFilterConfig())
+                                ->setOptions($options)
         );
     }
 
-    public static function ActionButtons($view=true,$edit=true,$delete=true,$edit2=false)
-    {
-      return   (new FieldConfig)
+    public static function ActionButtons($url, $view = true, $edit = true, $delete = true, $edit2 = false) {
+        return (new FieldConfig)
                         ->setName('id')
                         ->setLabel('Action')
-                        ->setCallback(function ($val, ObjectDataRow $row) use ($view,$edit,$edit2,$delete){
-                          $data = $row->getSrc();
-                          return view('admin.layouts.partial.actionbuttons', compact('val','data','view','edit','edit2','delete'));
+                        ->setCallback(function ($val, ObjectDataRow $row) use ($url, $view, $edit, $edit2, $delete) {
+                            $data = $row->getSrc();
+                            return view('admin.layouts.partial.actionbuttons', compact('url', 'val', 'data', 'view', 'edit', 'edit2', 'delete'));
                         });
     }
 
-    public static function Components()
-    {
-      return [
-          # Renders table header (table>thead)
-          (new THead)
-              # Setup inherited components
-              ->setComponents([
-                      (new ColumnHeadersRow),
-                  # Add this if you have filters for automatic placing to this row
-                  new FiltersRow,
-                  # Row with additional controls
-                  (new OneCellRow)
-                      ->setComponents([
-                          # Control for specifying quantity of records displayed on page
-                          (new RecordsPerPage)
-                              ->setVariants([
-                                  50,
-                                  100,
-                                  1000
-                              ])
-                          ,
-                          # Control to show/hide rows in table
-                         (new ColumnsHider),
-
-
-                          # Submit button for filters.
-                          # Place it anywhere in the grid (grid is rendered inside form by default).
-                          (new HtmlTag)
-                              ->setTagName('button')
-                              ->setAttributes([
-                                  'type' => 'submit',
-                                  # Some bootstrap classes
-                                  'class' => 'btn btn-primary'
-                              ])
-                              ->setContent('Filter')
-                      ])
-                      # Components may have some placeholders for rendering children there.
-                      ->setRenderSection(THead::SECTION_BEGIN)
-              ])
-          ,
-          # Renders table footer (table>tfoot)
-          (new TFoot)
-              // ->addComponent(
-              //     # TotalsRow component calculates totals on current page
-              //     # (max, min, sum, average value, etc)
-              //     # and renders results as table row.
-              //     # By default there is a sum.
-              //     new TotalsRow([
-              //         'comments',
-              //         'posts',
-              //     ])
-              // )
-              ->addComponent(
-                  # Renders row containing one cell
-                  # with colspan attribute equal to the table columns count
-                  (new OneCellRow)
-                      # Pagination control
-                      ->addComponent(new Pager)
-              )
-      ];
+    public static function Components() {
+        return [
+            # Renders table header (table>thead)
+                    (new THead)
+                    # Setup inherited components
+                    ->setComponents([
+                        (new ColumnHeadersRow),
+                        # Add this if you have filters for automatic placing to this row
+                        new FiltersRow,
+                        # Row with additional controls
+                        (new OneCellRow)
+                        ->setComponents([
+                            # Control for specifying quantity of records displayed on page
+                            (new RecordsPerPage)
+                            ->setVariants([
+                                50,
+                                100,
+                                1000
+                            ])
+                            ,
+                            # Control to show/hide rows in table
+                            (new ColumnsHider),
+                            # Submit button for filters.
+                            # Place it anywhere in the grid (grid is rendered inside form by default).
+                            (new HtmlTag)
+                            ->setTagName('button')
+                            ->setAttributes([
+                                'type' => 'submit',
+                                # Some bootstrap classes
+                                'class' => 'btn btn-primary'
+                            ])
+                            ->setContent('Filter')
+                        ])
+                        # Components may have some placeholders for rendering children there.
+                        ->setRenderSection(THead::SECTION_BEGIN)
+                    ])
+            ,
+            # Renders table footer (table>tfoot)
+            (new TFoot)
+                    // ->addComponent(
+                    //     # TotalsRow component calculates totals on current page
+                    //     # (max, min, sum, average value, etc)
+                    //     # and renders results as table row.
+                    //     # By default there is a sum.
+                    //     new TotalsRow([
+                    //         'comments',
+                    //         'posts',
+                    //     ])
+                    // )
+                    ->addComponent(
+                            # Renders row containing one cell
+                            # with colspan attribute equal to the table columns count
+                            (new OneCellRow)
+                            # Pagination control
+                            ->addComponent(new Pager)
+                    )
+        ];
     }
+
 }
--- src/views/actionbuttons.blade.php
+++ src/views/actionbuttons.blade.php
@@ -1,30 +1,30 @@
 
 @if( $view )
-  <a href="{{ url('/admin/orders/' . $val) }}" class="btn btn-success btn-xs" title="View Order">
+<a href="{{ url($url . '/' . $val) }}" class="btn btn-success btn-xs" title="View Order">
     <span class="glyphicon glyphicon-eye-open" aria-hidden="true"/>
-  </a>
+</a>
 @endif
 @if( $edit )
-  <a href="{{ url('/admin/orders/' . $val . '/edit') }}" class="btn btn-primary btn-xs" title="Edit Order">
+<a href="{{ url($url . '/' . $val . '/edit') }}" class="btn btn-primary btn-xs" title="Edit Order">
     <span class="glyphicon glyphicon-pencil" aria-hidden="true"/>
-  </a>
+</a>
 @endif
 @if( $edit2 )
-  <a href="{{ url('/admin/loans/' . $val . '/edit') }}" class="btn btn-success btn-xs" title="Loan">
+<a href="{{ url($url . '/' . $val . '/edit') }}" class="btn btn-success btn-xs" title="Loan">
     <span class="glyphicon glyphicon-list-alt" aria-hidden="true"/>
-  </a>
+</a>
 @endif
 @if( $delete )
-  {!! Form::open([
-    'method'=>'DELETE',
-    'url' => ['/admin/orders', $val],
-    'style' => 'display:inline'
-  ]) !!}
-  {!! Form::button('<span class="glyphicon glyphicon-trash" aria-hidden="true" title="Delete Order" />', array(
-    'type' => 'submit',
-    'class' => 'btn btn-danger btn-xs',
-    'title' => 'Delete Order',
-    'onclick'=>'return confirm("Confirm delete?")'
-  ));!!}
-  {!! Form::close() !!}
+{!! Form::open([
+'method'=>'DELETE',
+'url' => [$url, $val],
+'style' => 'display:inline'
+]) !!}
+{!! Form::button('<span class="glyphicon glyphicon-trash" aria-hidden="true" title="Delete Order" />', array(
+'type' => 'submit',
+'class' => 'btn btn-danger btn-xs',
+'title' => 'Delete Order',
+'onclick'=>'return confirm("Confirm delete?")'
+));!!}
+{!! Form::close() !!}
 @endif
